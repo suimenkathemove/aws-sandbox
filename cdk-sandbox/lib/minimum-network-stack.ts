c72c3627523ee6f17df8508fe89bd7da48819bac
suimenkathemove <yoshiharu2580@gmail.com>
2024-03-11 04:07:55 +0900

    add ingressSubnet routeTable

--- cdk-sandbox/lib/minimum-network-stack.ts
+++ cdk-sandbox/lib/minimum-network-stack.ts
@@ -11,37 +11,66 @@ export class MinimumNetworkStack extends cdk.Stack {
     super(scope, id, props);
 
     const vpc = ((): cdk.aws_ec2.CfnVPC => {
-      const id = `${DOMAIN_NAME}-vpc`;
-
-      return new cdk.aws_ec2.CfnVPC(this, id, {
+      const vpcId = `${DOMAIN_NAME}-vpc`;
+      const vpc = new cdk.aws_ec2.CfnVPC(this, vpcId, {
         cidrBlock: "10.0.0.0/16" satisfies CidrBlock,
-        tags: tags(id),
+        tags: tags(vpcId),
       });
+
+      return vpc;
     })();
 
-    const _ingressSubnet = ((): cdk.aws_ec2.CfnSubnet => {
-      const id = `${DOMAIN_NAME}-ingress-subnet`;
-
-      return new cdk.aws_ec2.CfnSubnet(this, id, {
-        vpcId: vpc.ref,
-        cidrBlock: "10.0.0.0/24" satisfies CidrBlock,
-        tags: tags(id),
-      });
-    })();
-
-    const _igw = ((): cdk.aws_ec2.CfnInternetGateway => {
-      const id = `${DOMAIN_NAME}-igw`;
-
-      const igw = new cdk.aws_ec2.CfnInternetGateway(this, id, {
-        tags: tags(id),
+    const igw = ((): cdk.aws_ec2.CfnInternetGateway => {
+      const igwId = `${DOMAIN_NAME}-igw`;
+      const igw = new cdk.aws_ec2.CfnInternetGateway(this, igwId, {
+        tags: tags(igwId),
       });
 
-      new cdk.aws_ec2.CfnVPCGatewayAttachment(this, `${id}-attachment`, {
+      new cdk.aws_ec2.CfnVPCGatewayAttachment(this, `${igwId}-attachment`, {
         vpcId: vpc.ref,
         internetGatewayId: igw.ref,
       });
 
       return igw;
+    })();
+
+    const _ingressSubnetAndRouteTable = ((): {
+      subnet: cdk.aws_ec2.CfnSubnet;
+      routeTable: cdk.aws_ec2.CfnRouteTable;
+    } => {
+      const subnetId = `${DOMAIN_NAME}-ingress-subnet`;
+      const subnet = new cdk.aws_ec2.CfnSubnet(this, subnetId, {
+        vpcId: vpc.ref,
+        cidrBlock: "10.0.0.0/24" satisfies CidrBlock,
+        tags: tags(subnetId),
+      });
+
+      const routeTable = ((): cdk.aws_ec2.CfnRouteTable => {
+        const routeTableId = `${subnetId}-route-table`;
+        const routeTable = new cdk.aws_ec2.CfnRouteTable(this, routeTableId, {
+          vpcId: vpc.ref,
+          tags: tags(routeTableId),
+        });
+
+        new cdk.aws_ec2.CfnSubnetRouteTableAssociation(
+          this,
+          `${routeTableId}-association`,
+          {
+            subnetId: subnet.ref,
+            routeTableId: routeTable.ref,
+          },
+        );
+
+        new cdk.aws_ec2.CfnRoute(this, `${DOMAIN_NAME}-default-gateway-route`, {
+          routeTableId: routeTable.ref,
+          destinationCidrBlock: "0.0.0.0/0" satisfies CidrBlock,
+          gatewayId: igw.ref,
+        });
+
+        return routeTable;
+      })();
+
+      return { subnet, routeTable };
     })();
   }
 }
